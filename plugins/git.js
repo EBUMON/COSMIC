92ca33b672e4cf4799287893b33d061095f798c8
ANUSER <93968310+ANUSER1@users.noreply.github.com>
2022-01-22 10:26:41 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -10,15 +10,15 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
     var skl = await axios.get("https://telegra.ph/file/5b1a13030d82fce99f12f.jpg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `RDX Bot Created By Abhiram Sachu
-Creator number 👉: wa.me/917736771516
+Creator number 👉: wa.me/916238560742
 
-Creator number 👇: https://wa.me/qr/62FORLJKJZ4WP1
 
-Githublink (Setup)  👇:    https://github.com/KINGS-AS/RDX
+
+Githublink (Setup)  👇:    https://github.com/ANUSER1/ADBOT
 
 QR       👇:  https://replit.com/@phaticusthiccy/WhatsAsena-QR
 
-DEPLOYE  👇:  https://heroku.com/deploy?template=https://github.com/KINGS-AS/RDX.git
+DEPLOYE  👇:  https://heroku.com/deploy?template=https://github.com/ANUSER1/ADBOT.git
 
 AS BOT GROUP 👇: https://chat.whatsapp.com/CaFM7tvnK3TLs8UX0oQWT7
 

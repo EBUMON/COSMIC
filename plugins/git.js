fad2263f664b6dcebfd6d9132dff0bbe735a121f
ANSHID <93968310+ANUSER1@users.noreply.github.com>
2022-01-24 12:26:18 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,9 +7,9 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://telegra.ph/file/5b1a13030d82fce99f12f.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.ibb.co/KWyM3Qw/COSMIC.jpg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `RDX Bot Created By Abhiram Sachu
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `𝑪𝒐𝒔𝒎𝒊𝒄 𝒃𝒐𝒕 𝒄𝒓𝒆𝒂𝒕𝒆𝒅 𝒃𝒚 𝒂𝒏𝒔𝒉𝒊𝒅
 Creator number 👉: wa.me/916238560742
 
 Githublink (Setup)  👇:    https://github.com/ANUSER1/COSMIC

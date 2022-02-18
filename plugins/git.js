25dd393d39732d71a28dca6a432034b174a99b4a
KHADHER <96767020+Khadher05@users.noreply.github.com>
2022-02-18 17:17:10 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -10,13 +10,13 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
     var skl = await axios.get("https://i.ibb.co/KWyM3Qw/COSMIC.jpg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `𝑪𝒐𝒔𝒎𝒊𝒄 𝒃𝒐𝒕 𝒄𝒓𝒆𝒂𝒕𝒆𝒅 𝒃𝒚 𝒂𝒏𝒔𝒉𝒊𝒅
-Creator number 👉: wa.me/916238560742
+Creator number 👉: wa.me/917025191792
 
-Githublink (Setup)  👇:    https://github.com/ANUSER1/COSMIC
+Githublink (Setup)  👇:    https://github.com/khadher05/COSMIC
 
-DEPLOYE  👇:  https://heroku.com/deploy?template=https://github.com/ANUSER1/COSMIC.git
+DEPLOYE  👇:  https://heroku.com/deploy?template=https://github.com/khadher05/COSMIC.git
 
-PUBLIC BOT GROUP 👇: https://chat.whatsapp.com/KeEMhqBW644IN46PYsGDWf
+PUBLIC BOT GROUP 👇: https://chat.whatsapp.com/FhPPp43dYlNLi499ZJuq5X
 
 
 `}) 

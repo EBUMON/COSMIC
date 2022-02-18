ce817af615540c9f429279d26bbb4d035b895b4f
KHADHER <96767020+Khadher05@users.noreply.github.com>
2022-02-18 13:05:38 +0530

    Update number

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '6281334177340', 'Nurutomo', m)
+  // F this.sendContact(m.chat, '917025191792', 'Nurutomo', m)
+  this.sendContact(m.chat, '917025191792', 'Nurutomo', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
